563916ebc475c3a1ea5dbbad430131d0957e5852
ias1234 <161636995+ias1234@users.noreply.github.com>
2024-03-23 17:40:17 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "15052451"
 api_hash = "dbf8fdfc66d7a1a9bf359c036409aa14"
-bot_token = "6836055340:AAGpASjBYBKv5j2Q-lhgo8P5-PLZDZCWQRE"
+bot_token = "7121473749:AAFlpPQUegkVFmCVJfTcIrlI5U7Lq6-B7_U"
